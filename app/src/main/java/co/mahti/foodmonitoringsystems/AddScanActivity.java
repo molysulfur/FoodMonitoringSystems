e86381dae6dd79a785b9670e34a805f740ab982b
timmxm <thanongsak_n@cmu.ac.th>
2018-08-29 17:25:30 +0700

    AddScan, Settings, added + link to main activity

---
+++ app/src/main/java/co/mahti/foodmonitoringsystems/AddScanActivity.java
@@ -0,0 +1,13 @@
+package co.mahti.foodmonitoringsystems;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddScanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_scan);
+    }
+}
