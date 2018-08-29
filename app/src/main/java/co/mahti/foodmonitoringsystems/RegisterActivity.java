93a1989a20d1481957fdeee15b83fe4c6323c533
timmxm <thanongsak_n@cmu.ac.th>
2018-08-29 17:07:36 +0700

    test flag on register

--- app/src/main/java/co/mahti/foodmonitoringsystems/RegisterActivity.java
+++ app/src/main/java/co/mahti/foodmonitoringsystems/RegisterActivity.java
@@ -4,9 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class RegisterActivity extends AppCompatActivity {
+    boolean flag = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        flag = false;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
     }
